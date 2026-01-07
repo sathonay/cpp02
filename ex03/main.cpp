4d5b4bdd9e40707d50d1c719f3cd4ab27ecdbeb1
Alexis <33222808+sathonay@users.noreply.github.com>
2026-01-07 15:27:43 +0100

    replace bsp test

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -12,9 +12,9 @@ int main() {
 		Point(0.0f, 1.0f),
 	};
 
+	std::cout << bsp(points[0], points[1], points[2], Point(0.0f, -0.25f)) << std::endl;
+	std::cout << bsp(points[0], points[1], points[2], Point(-0.25f, 0.0f)) << std::endl;
 	std::cout << bsp(points[0], points[1], points[2], Point(0.25f, 0.25f)) << std::endl;
+	std::cout << bsp(points[0], points[1], points[2], Point(0.49f, 0.49f)) << std::endl;
 	std::cout << bsp(points[0], points[1], points[2], Point(0.5f, 0.5f)) << std::endl;
-	std::cout << bsp(points[0], points[1], points[2], Point(0.51f, 0.51f)) << std::endl;
-	std::cout << bsp(points[0], points[1], points[2], Point(0.502f, 0.502f)) << std::endl;
-	std::cout << bsp(points[0], points[1], points[2], Point(0.5019f, 0.5019f)) << std::endl;
 }
